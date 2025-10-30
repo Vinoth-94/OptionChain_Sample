f2413c05d8edfdc9394e082acce1934fe9943c2e
Vinoth-94 <144323939+Vinoth-94@users.noreply.github.com>
2025-10-30 21:42:54 +0530

    package json updated  commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   server: {
     // This section tells Vite to proxy requests starting with /api/nse
     proxy: {
